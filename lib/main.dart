7ce2070963d90b1a3ccb0378d4e1969f5914de20
SHA3L <mr.sahil.in@gmail.com>
2025-05-13 18:40:32 +1200

    lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  runApp(FabSuiteApp());
+}
+
+class FabSuiteApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FabSuite AI',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
